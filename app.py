31df64ab69058157f1996919b970644a2bcd5b4c
Medhaa-Naik <medhaa.m.n@gmail.com>
2026-02-05 23:27:10 +0530

    added rows to jobs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify
 from database import load_jobs_from_db,create_tables
 from sqlalchemy import text
+from database import engine
 
 
 app = Flask(__name__)
@@ -18,6 +19,21 @@ def homepage():
 def list_jobs():
     jobs=load_jobs_from_db()
     return jsonify(jobs)
+
+@app.route("/seed")
+def seed():
+    with engine.connect() as conn:
+        conn.execute(text("""
+            INSERT INTO jobs (title, location, responsibilities, requirements)
+            VALUES
+            ('Data Analyst', 'Bengaluru', 'Analyze data and create reports', 'Python, SQL, Excel'),
+            ('Data Scientist', 'Delhi', 'Build ML models', 'Python, ML, Statistics'),
+            ('Frontend Developer', 'Mumbai', 'Build UI', 'HTML, CSS, JavaScript, React'),
+            ('Backend Developer', 'Hyderabad', 'Build APIs', 'Flask, PostgreSQL')
+            """))
+        conn.commit()
+        return "jobs added succesfully!"
+
 
 
 if __name__ =="__main__":
